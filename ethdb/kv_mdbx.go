623387d6868de5393f43b5c113da39f4cbe67111
alex.sharov <AskAlexSharov@gmail.com>
2020-11-19 16:59:54 +0700

    save progress

--- ethdb/kv_mdbx.go
+++ ethdb/kv_mdbx.go
@@ -123,7 +123,7 @@ func (opts MdbxOpts) Open() (KV, error) {
 		return nil, fmt.Errorf("could not create dir: %s, %w", opts.path, err)
 	}
 
-	var flags uint = mdbx.NoReadahead
+	var flags uint = 0
 	if opts.readOnly {
 		flags |= mdbx.Readonly
 	}
