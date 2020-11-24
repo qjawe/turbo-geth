4bcc7542156f042cc422a9c9ee16512378a7ed64
alex.sharov <AskAlexSharov@gmail.com>
2020-11-24 12:28:41 +0700

    do appned for dupsort buckets

--- core/state/plain_state_writer.go
+++ core/state/plain_state_writer.go
@@ -156,7 +156,7 @@ func (w *PlainStateWriter) WriteChangeSets() error {
 	var prevK []byte
 	if err = changeset.Mapper[dbutils.PlainAccountChangeSetBucket].Encode(w.blockNumber, accountChanges, func(k, v []byte) error {
 		if bytes.Equal(k, prevK) {
-			if err := db.(*ethdb.TxDb).AppendDup(dbutils.PlainAccountChangeSetBucket, k, v); err != nil {
+			if err := db.AppendDup(dbutils.PlainAccountChangeSetBucket, k, v); err != nil {
 				return err
 			}
 		} else {
