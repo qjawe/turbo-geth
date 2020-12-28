e679ffb78c997d0a312c26b4454c3d9b8140f56d
alex.sharov <AskAlexSharov@gmail.com>
2020-12-28 11:15:25 +0700

    1

--- eth/stagedsync/stage_interhashes.go
+++ eth/stagedsync/stage_interhashes.go
@@ -33,7 +33,7 @@ func SpawnIntermediateHashesStage(s *StageState, db ethdb.Database, checkRoot bo
 		s.Done()
 		return nil
 	}
-	fmt.Printf("%d->%d\n", s.BlockNumber, to)
+	//fmt.Printf("%d->%d\n", s.BlockNumber, to)
 	var tx ethdb.DbWithPendingMutations
 	var useExternalTx bool
 	if hasTx, ok := db.(ethdb.HasTx); ok && hasTx.Tx() != nil {
