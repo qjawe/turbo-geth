1db0284fcddbc52908340bef2a59fca80165ba86
alex.sharov <AskAlexSharov@gmail.com>
2021-02-09 15:43:22 +0700

    more info

--- ethdb/kv_mdbx.go
+++ ethdb/kv_mdbx.go
@@ -123,7 +123,7 @@ func (opts MdbxOpts) Open() (KV, error) {
 		}
 	}
 
-	if err = env.SetGeometry(-1, -1, int(opts.mapSize), int(5*datasize.GB), -1, -1); err != nil {
+	if err = env.SetGeometry(-1, -1, int(opts.mapSize), int(4*datasize.GB), -1, -1); err != nil {
 		return nil, err
 	}
 
