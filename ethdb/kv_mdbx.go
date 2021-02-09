28aa030d46760e6e954f80c75afbfa3dd735f8c9
alex.sharov <AskAlexSharov@gmail.com>
2021-02-09 19:06:41 +0700

    delete old code from hack.go

--- ethdb/kv_mdbx.go
+++ ethdb/kv_mdbx.go
@@ -51,7 +51,7 @@ type MdbxOpts struct {
 func NewMDBX() MdbxOpts {
 	return MdbxOpts{
 		bucketsCfg: DefaultBucketConfigs,
-		flags:      mdbx.NoReadahead | mdbx.Coalesce | mdbx.Durable,
+		flags:      mdbx.NoReadahead | mdbx.Coalesce | mdbx.Durable | mdbx.LifoReclaim,
 	}
 }
 
