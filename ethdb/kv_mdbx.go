0b58632230bc1b60a235cafae9235065ed8bc9cf
alex.sharov <AskAlexSharov@gmail.com>
2021-02-11 13:59:20 +0700

    merge

--- ethdb/kv_mdbx.go
+++ ethdb/kv_mdbx.go
@@ -135,11 +135,6 @@ func (opts MdbxOpts) Open() (KV, error) {
 		return nil, err
 	}
 
-	err = env.SetOption(mdbx.OptTxnDpLimit, 128*1024) // default: 64*1024
-	if err != nil {
-		return nil, err
-	}
-
 	if opts.maxFreelistReuse == 0 {
 		opts.maxFreelistReuse = LMDBDefaultMaxFreelistReuse
 	}
@@ -157,6 +152,10 @@ func (opts MdbxOpts) Open() (KV, error) {
 	err = env.Open(opts.path, flags, 0664)
 	if err != nil {
 		return nil, fmt.Errorf("%w, path: %s", err, opts.path)
+	}
+	err = env.SetOption(mdbx.OptTxnDpLimit, 128*1024) // default: 64*1024
+	if err != nil {
+		return nil, err
 	}
 
 	db := &MdbxKV{
