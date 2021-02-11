1a0e79c2b0aa6789f2d591d542d66de1f257a543
alex.sharov <AskAlexSharov@gmail.com>
2021-02-11 14:01:23 +0700

    clean

--- ethdb/kv_mdbx.go
+++ ethdb/kv_mdbx.go
@@ -100,12 +100,7 @@ func (opts MdbxOpts) Open() (KV, error) {
 		return nil, err
 	}
 
-	err = env.SetOption(mdbx.OptRpAugmentLimit, 32*1024*1024)
-	if err != nil {
-		return nil, err
-	}
-
-	_ = env.SetDebug(mdbx.LogLvlExtra, mdbx.DbgAssert, mdbx.LoggerDoNotChange) // temporary disable error, because it works if call it 1 time, but returns error if call it twice in same process (what often happening in tests)
+	//_ = env.SetDebug(mdbx.LogLvlExtra, mdbx.DbgAssert, mdbx.LoggerDoNotChange) // temporary disable error, because it works if call it 1 time, but returns error if call it twice in same process (what often happening in tests)
 
 	err = env.SetMaxDBs(100)
 	if err != nil {
@@ -153,6 +148,11 @@ func (opts MdbxOpts) Open() (KV, error) {
 	if err != nil {
 		return nil, fmt.Errorf("%w, path: %s", err, opts.path)
 	}
+	err = env.SetOption(mdbx.OptRpAugmentLimit, 32*1024*1024)
+	if err != nil {
+		return nil, err
+	}
+
 	err = env.SetOption(mdbx.OptTxnDpLimit, 128*1024) // default: 64*1024
 	if err != nil {
 		return nil, err
