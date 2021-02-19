1076adf2e398151c3aa740479bc7725601b7d53c
alex.sharov <AskAlexSharov@gmail.com>
2021-02-19 21:46:42 +0700

    squash

--- ethdb/kv_mdbx.go
+++ ethdb/kv_mdbx.go
@@ -144,16 +144,16 @@ func (opts MdbxOpts) Open() (KV, error) {
 	// 1/8 is good for transactions with a lot of modifications - to reduce invalidation size.
 	// But TG app now using Batch and etl.Collectors to avoid writing to DB frequently changing data.
 	// It means most of our writes are: APPEND or "single UPSERT per key during transaction"
-	if err = env.SetOption(mdbx.OptSpillMinDenominator, 8); err != nil {
+	if err = env.SetOption(mdbx.OptSpillMinDenominator, 4); err != nil {
 		return nil, err
 	}
 	//if err = env.SetOption(mdbx.OptSpillMaxDenominator, 0); err != nil {
 	//	return nil, err
 	//}
-	if err = env.SetOption(mdbx.OptTxnDpInitial, 16*1024); err != nil {
+	if err = env.SetOption(mdbx.OptTxnDpInitial, 4*1024); err != nil {
 		return nil, err
 	}
-	if err = env.SetOption(mdbx.OptDpReverseLimit, 16*1024); err != nil {
+	if err = env.SetOption(mdbx.OptDpReverseLimit, 4*1024); err != nil {
 		return nil, err
 	}
 	if err = env.SetOption(mdbx.OptTxnDpLimit, 128*1024); err != nil {
